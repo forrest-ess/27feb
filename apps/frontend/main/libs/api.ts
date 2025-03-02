9ceb852f042c9710553f6b06a6af5fb5494c3a99
Forrest <forrest.ess@gmail.com>
2025-03-02 17:01:51 +0900

    change api url

--- apps/frontend/main/libs/api.ts
+++ apps/frontend/main/libs/api.ts
@@ -3,7 +3,8 @@ import axios from 'axios';
 const api = axios.create({
   baseURL:
     // 'https://mc-isaac-conviction-submitted.trycloudflare.com/api',
-    'http://localhost:9000/api',
+    // 'http://localhost:9000/api',
+    'https://common-api.daataa.dev',
 });
 
 export const registerUser = async (user: {
